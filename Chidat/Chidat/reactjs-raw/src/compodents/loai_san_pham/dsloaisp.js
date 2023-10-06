eb1f169f440f11eb85358668613f43e2b3de9542
dat0335405877 <0306211446@caothang.edu.vn>
2023-10-06 23:18:33 +0700

    first commit

---
+++ Chidat/Chidat/reactjs-raw/src/compodents/loai_san_pham/dsloaisp.js
@@ -0,0 +1,59 @@
+const dsLoaiSanPham=[
+    {
+        'ten':'HP',
+        'ds_san_pham':[
+            {
+                'idsp1':'1',
+                'hinh_anh':'img_products/HPenvy13.jpg',
+                'ten':'HP Envy 13',
+                'gia': 13000000
+            },
+            {
+                'idsp1':'2',
+                'hinh_anh':'img_products/HPenvy15.jpg',
+                'ten':'HP Envy 15',
+                'gia': 20000000
+            },
+            {
+                'idsp1':'3',
+                'hinh_anh':'img_products/HPPavilion14.jpg',
+                'ten':'HP Pavilion 14',
+                'gia': 10000000
+            },
+            {
+                'idsp1':'4',
+                'hinh_anh':'img_products/HPPavilion15.jpg',
+                'ten':'HP Pavilion 15',
+                'gia': 15000000
+            },
+            {
+                'idsp1':'5',
+                'hinh_anh':'img_products/HPProBook440.jpg',
+                'ten':'HP ProBook 440',
+                'gia': 25000000
+            }
+            
+        ]
+       
+    },
+    {
+        'ten':'MSI',
+        'ds_san_pham':[
+            {
+                'idsp2':'1',
+                'hinh_anh':'img_products/MSIGamingGF63.jpg',
+                'ten':' MSI Gaming GF63',
+                'gia': 24000000
+            },
+            {
+                'idsp2':'2',
+                'hinh_anh':'img_products/MSIModern14.jpg',
+                'ten':'MSI Modern 14',
+                'gia': 20000000
+            }
+            
+        ]
+    }
+];
+
+export default dsLoaiSanPham;
---
+++ Chidat/Chidat/reactjs-raw/src/compodents/main.js
@@ -0,0 +1,10 @@
+function Main(){
+    return(
+        <main>
+            <div>
+            <img src="img_products/avatar.jpg" alt="" width="1550" height="400"></img>
+            </div>
+        </main>
+    )
+}
+export default Main; 
